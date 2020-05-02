807041e55e69e7b986954ca845db479b084005c4
Jose Illan <jose_illan@hotmail.com>
2020-05-03 00:32:38 +0200

    POST & PUT and list all elements

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from 'src/app/services/heroes.service';
+
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe: HeroeModel = new HeroeModel();
+
+  constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
+  }
+
+  guardar( form: NgForm ){
+    if( form.invalid ) {
+      console.log('Formulario no válido');
+      return;
+    }
+
+    Swal.fire({
+      title: 'Espere',
+      icon: 'info',
+      text: 'Guardando información',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if ( this.heroe.id ){
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
+    } else {
+      peticion = this.heroesService.crearHeroe( this.heroe );
+    }
+    
+    peticion.subscribe( resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        icon: 'success'
+      });
+    })
   }
 
 }
